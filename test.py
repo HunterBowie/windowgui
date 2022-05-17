d41f0815f6cbb58bda6725272bcced867d33bfaf
Hunter Bowie (Student) <1269473@learn.vsb.bc.ca>
2022-05-17 10:19:26 -0700

    minor testing

--- test.py
+++ test.py
@@ -7,30 +7,47 @@ windowgui.Text.default_style["font_file"] = path.join(windowgui.FOUNTS_DIR, "rou
 win = windowgui.Window((800, 800))
 win.bg_color = windowgui.Colors.WHITE
 
-my_button = windowgui.Button("my-btn", 10, 10, 100, 100, color_style=windowgui.UIColorStyle.BLUE, top_img=windowgui.load_image("audioOn", windowgui.IMAGES_DIR, scale=(50, 50), convert=False, colorkey=None))
-my_textbox = windowgui.TextBox("my-textbox", 100, 150, 300, 50, text_style={"color": windowgui.Colors.BLUE})
-my_slider = windowgui.Slider("my-slider", 300, 400, 200, 75, color_style=windowgui.UIColorStyle.BLUE)
-my_checkbox = windowgui.CheckBox("my-checkbox", 10, 600, 35, 35, color_style=windowgui.UIColorStyle().BLUE, checked=True)
-win.managers["ui"].add(my_button)
-win.managers["ui"].add(my_textbox)
-win.managers["ui"].add(my_slider)
-win.managers["ui"].add(my_checkbox)
-
-
-win.managers["flash"].add(
-windowgui.Flash(130, 0, (400, 50), windowgui.Text(0, 0, "Please provide valid input!", style={"size": 20}), windowgui.Colors.LIGHT_BLUE)
-)
-class Manager:
+class MyUI:
     def __init__(self, window):
         self.window = window
+        self.window.managers["ui"].clear()
+        my_button = windowgui.Button("my-btn", 10, 10, 100, 100, color_style=windowgui.UIColorStyle.BLUE, top_img=windowgui.load_image("audioOn", windowgui.IMAGES_DIR, scale=(50, 50), convert=False, colorkey=None))
+        my_textbox = windowgui.TextBox("my-textbox", 100, 150, 300, 50, text_style={"color": windowgui.Colors.BLUE})
+        my_slider = windowgui.Slider("my-slider", 300, 400, 200, 75, color_style=windowgui.UIColorStyle.BLUE)
+        my_checkbox = windowgui.CheckBox("my-checkbox", 10, 600, 35, 35, color_style=windowgui.UIColorStyle().BLUE, checked=True)
+        window.managers["ui"].add(my_button)
+        window.managers["ui"].add(my_textbox)
+        window.managers["ui"].add(my_slider)
+        window.managers["ui"].add(my_checkbox)
     
     def eventloop(self, event):
-        if event.type in windowgui.UIEvent.ALL:
-            self.window.managers["flash"].add(
-                windowgui.Flash(100, 0, (400, 50), windowgui.Text(0, 0, "Please provide valid input!", style={"size": 20}), windowgui.Colors.LIGHT_BLUE)
-            )
+        if event.type == windowgui.UIEvent.CHECKBOX_CLICKED:
+            print("buon is good stuff")
+        
+        if event.type == windowgui.UIEvent.BUTTON_CLICKED:
+            self.window.managers["main"] = MyOtherUI(self.window)
+    
+class MyOtherUI:
+    def __init__(self, window):
+        self.window = window
+        self.window.managers["ui"].clear()
+        my_textbox = windowgui.TextBox("my-textbox", 200, 150, 300, 50, text_style={"color": windowgui.Colors.RED})
+        self.window.managers["ui"].add(my_textbox)
+    
+    def eventloop(self, event):
+        if event.type == windowgui.UIEvent.TEXTBOX_POSTED:
+            print("ho yeah")
 
-win.managers["main"] = Manager(win)
-win.start(auto_cycle=True)
 
+        
+
+
+win.managers["main"] = MyUI(win)
+
+win.start(auto_cycle=False)
+
+while win.running:
+    for event in pygame.event.get():
+        win.eventloop(event)
+    win.update()
 
