52cf1cb3bce7a0b756490e93a06c85eb5734cc35
HunterBowie <73845280+HunterBowie@users.noreply.github.com>
2022-05-18 07:16:47 -0700

    update test.py

--- test.py
+++ test.py
@@ -1,32 +1,3 @@
-<<<<<<< HEAD
-import windowgui, pygame, time
-pygame.init()
-
-
-SCREEN_SIZE = 600, 600
-win = windowgui.Window(SCREEN_SIZE)
-win.bg_color = windowgui.Colors.WHITE
-
-pygame.display.set_caption("hello")
-
-
-win.start(auto_cycle=False)
-
-text = windowgui.Text(10, 10, "Forever")
-win.managers["ui"].ui.append(windowgui.ui.Button("btn", 250, 250, 200, 50, top_img=text.surface))
-win.managers["ui"].ui.append(windowgui.ui.TextBox("txtbx", 150, 450, 200, 50))
-
-while win.running:
-    for event in pygame.event.get():
-        win.eventloop(event)
-
-        if event.type == windowgui.Event.BUTTON_CLICK:
-            pass
-
-    win.update(auto_eventloop=False)
-
-win.end()
-=======
 import windowgui, pygame
 from os import path
 pygame.init()
@@ -66,7 +37,6 @@ class MyOtherUI:
     def eventloop(self, event):
         if event.type == windowgui.UIEvent.TEXTBOX_POSTED:
             print("ho yeah")
->>>>>>> d41f0815f6cbb58bda6725272bcced867d33bfaf
 
 
         
@@ -80,4 +50,3 @@ while win.running:
     for event in pygame.event.get():
         win.eventloop(event)
     win.update()
-
